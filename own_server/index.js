54ec4cd23878ebc45dabeadbdd6d42707d59af30
Muhammad Ikrash <mikrash62@gmail.com>
2026-07-08 14:12:39 +0500

    Making  Own node Js Server

---
+++ own_server/index.js
@@ -0,0 +1,22 @@
+import http from "http"
+import * as fs from 'node:fs';
+const server = http.createServer((req, res) => {
+    const log = `${Date.now()} : ${req.url} New Request Rec ! \n`
+    fs.appendFile("userinfo.txt", log, () => {
+        console.log("New Req Rec !");
+        switch (req.url) {
+            case "/":
+                res.end('Hello World Home!');
+                break;
+            case "/about":
+                res.end('Hello World About !');
+                break
+            default:
+                res.end("404 Error !")
+        }
+    })
+})
+
+const Port = 3000;
+server.listen(Port, () => console.log("Server Started Successful 👼")
+)
